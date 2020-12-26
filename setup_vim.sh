7f52f8d752a814372a125d97f4e48ae328678b3d
Khanh Nguyen <nmkhanhbk@gmail.com>
2020-12-26 05:48:08 +0000

    Support vim-go

---
+++ setup_vim.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Make sure GOPATH is set
+if [[ -z $GOPATH ]]
+then
+	echo "Empty GOPATH"
+	exit 1
+fi
+
+# Make sure GOBIN is set
+if [[ -z $GOBIN ]]
+then
+	echo "Empty GOBIN"
+	exit 1
+fi
+
+# Check if PATH includes GOBIN
+echo $PATH | grep -q $GOBIN
+if [[ $? -ne 0 ]]
+then
+	export PATH=$PATH:$GOBIN
+fi
+
+# Check sample vimrc file
+if [[ $# -ne 1 ]]
+then
+    echo "Please specify sample vimrc path"
+    exit 1
+fi
+
+# Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Copy vimrc
+cp $1 ~/.vimrc
+
+# Install plugin
+vim +PlugInstall +qall
+vim +GoInstallBinaries +qall
